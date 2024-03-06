37f1cb7ce636a54327bd09e76632c8290387ab0d
saniton <saniton7navelkar@gmail.com>
2024-03-06 09:23:54 +0530

    scanbooth

--- event/src/CBooth/ScanBoothQR.js
+++ event/src/CBooth/ScanBoothQR.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ScanQR() {
   return (
     <div style={{color: '#fff'}}>
-      <h2>Scan Your Booth QR code and login to see your details.</h2>
+      <h2>Scan Your Booth QR code to see your details.</h2>
     </div>
   )
 }
